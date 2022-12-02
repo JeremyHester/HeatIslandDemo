6ad659df63511a1f6d2a18a084bc926c78fce6ff
JeremyHester <119526734+JeremyHester@users.noreply.github.com>
2022-12-02 02:50:15 -0600

    Update heatmap.py

--- apps/heatmap.py
+++ apps/heatmap.py
@@ -6,7 +6,7 @@ def app():
 
     st.title("Heatmap")
 
-    filepath = "https://raw.githubusercontent.com/giswqs/leafmap/master/examples/data/us_cities.csv"
+    filepath = "https://raw.githubusercontent.com/JeremyHester/HeatIslandDemo/master/preliminarydata.csv"
     m = leafmap.Map(tiles="stamentoner")
     m.add_heatmap(
         filepath,
