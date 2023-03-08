23f2af09d4645f2decf2a409dd36372b881b3741
JeremyHester <119526734+JeremyHester@users.noreply.github.com>
2023-03-07 19:36:32 -0600

    Update heatmap.py

--- apps/heatmap.py
+++ apps/heatmap.py
@@ -1,5 +1,5 @@
 import streamlit as st
-import leafmap.foliumap as leafmap
+import leafmap.leafmap as leafmap
 
 
 def app():
@@ -7,13 +7,15 @@ def app():
     st.title("Heatmap")
 
     filepath = "https://raw.githubusercontent.com/JeremyHester/HeatIslandDemo/master/preliminarydata.csv"
-    m = leafmap.Map(tiles="stamentoner")
-    m.add_heatmap(
-        filepath,
-        latitude="latitude",
-        longitude="longitude",
-        value="pop_max",
-        name="Heat map",
-        radius=20,
+   # m = leafmap.Map(tiles="stamentoner")
+m = leafmap.Map()
+m.add_basemap("Stamen.Toner")
+m.add_heatmap(
+    filepath,
+    latitude="latitude",
+    longitude='longitude',
+    value="pop_max",
+    name="Heat map",
+    radius=20,
     )
     m.to_streamlit(height=700)
