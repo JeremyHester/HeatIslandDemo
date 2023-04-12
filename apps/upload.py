e6516bcd3bca236504931ea7a000ebf70b8088e5
JeremyHester <119526734+JeremyHester@users.noreply.github.com>
2023-04-11 22:04:24 -0500

    Update upload.py

--- apps/upload.py
+++ apps/upload.py
@@ -1,24 +1,7 @@
 import os
-import geopandas as gpd
+#import geopandas as gpd
 import streamlit as st
 import pandas as pd
-
-def save_uploaded_file(file_content, file_name):
-    """
-    Save the uploaded file to a temporary directory
-    """
-    import tempfile
-    import os
-    import uuid
-
-    _, file_extension = os.path.splitext(file_name)
-    file_id = str(uuid.uuid4())
-    file_path = os.path.join(tempfile.gettempdir(), f"{file_id}{file_extension}")
-
-    with open(file_path, "wb") as file:
-        file.write(file_content.getbuffer())
-
-    return file_path
 
 
 def app():
