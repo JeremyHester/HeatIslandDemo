87a52cb132d05bb36478490d29cac23a61cf28f6
JeremyHester <119526734+JeremyHester@users.noreply.github.com>
2023-04-12 03:27:31 -0500

    Update heatmap.py

--- apps/heatmap.py
+++ apps/heatmap.py
@@ -13,15 +13,15 @@ def app():
    # Create a correlation matrix from the data
    corr_matrix = data.corr()
 
-   # Create a heatmap using Plotly
-   fig = px.imshow(corr_matrix, x=corr_matrix.columns, y=corr_matrix.columns)
+  # Create a Plotly scattermapbox object
+   map_fig = px.scatter_mapbox(data, lat='latitude', lon='longitude', color=corr_matrix)
 
    # Add hover information
-   fig.update_layout(title='Correlation Matrix Heatmap', xaxis_title='Columns', yaxis_title='Columns')
-   fig.update_traces(hovertemplate='Column X: %{x}<br>Column Y: %{y}<br>Correlation: %{z:.2f}')
+   map_fig.update_layout(title='Correlation Matrix Heatmap on Map', mapbox_style='open-street-map')
+   map_fig.update_traces(hovertemplate='Latitude: %{lat}<br>Longitude: %{lon}<br>Correlation: %{marker.color:.2f}')
 
    # Display the plot in Streamlit
-   st.plotly_chart(fig, use_container_width=True)
+   st.plotly_chart(map_fig, use_container_width=True)
    
    
    
