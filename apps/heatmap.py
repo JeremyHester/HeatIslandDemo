b028da04fc3100d03b9bf82167901b6cafaf72cd
JeremyHester <119526734+JeremyHester@users.noreply.github.com>
2023-04-12 02:17:55 -0500

    Update heatmap.py

--- apps/heatmap.py
+++ apps/heatmap.py
@@ -1,7 +1,7 @@
 import os 
 import streamlit as st
 import matplotlib as mp
-import leafmap.leafmap as leafmap
+#import leafmap.leafmap as leafmap
 import pandas as pd
 import seaborn as sns
 
@@ -10,26 +10,29 @@ def app():
    st.title("Heatmap")
    filepath = "https://raw.githubusercontent.com/JeremyHester/HeatIslandDemo/master/preliminarydata.csv"
    # Read the CSV file into a DataFrame
-      #df = pd.read_csv(filepath)
+     df = pd.read_csv(filepath)
    # Generate a correlation matrix for the DataFrame
-      #corr_matrix = df.corr()
+     corr_matrix = df.corr()
    # Create a heatmap of the correlation matrix using Seaborn
-      #fig, ax = plt.subplots(figsize=(10,10))
-      #sns.heatmap(corr_matrix, annot=True, cmap='coolwarm', ax=ax)
+     fig, ax = plt.subplots(figsize=(10,10))
+     sns.heatmap(corr_matrix, annot=True, cmap='coolwarm', ax=ax)
    # Display the heatmap in the app
-      #st.pyplot(fig)
-   m = leafmap.Map(tiles="stamentoner")
-   m = leafmap.Map()
-   m.add_basemap("Stamen.Toner")
-   m.add_heatmap(
-       filepath,
-       latitude="latitude",
-       longitude='longitude',
-       value="temperature",
-       name="Heat map",
-       radius=20,
-    )
-   m.to_streamlit(height=700)
+     st.pyplot(fig)
+   
+   
+   
+   #m = leafmap.Map(tiles="stamentoner")
+   #m = leafmap.Map()
+   #m.add_basemap("Stamen.Toner")
+   #m.add_heatmap(
+    #   filepath,
+     #  latitude="latitude",
+      # longitude='longitude',
+       #value="temperature",
+      # name="Heat map",
+      # radius=20,
+    #)
+   #m.to_streamlit(height=700)
 
     
     
