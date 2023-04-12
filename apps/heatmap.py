90e444be25868d299da0ab5194c71b8b7632cbe2
JeremyHester <119526734+JeremyHester@users.noreply.github.com>
2023-04-12 02:11:27 -0500

    Update heatmap.py

--- apps/heatmap.py
+++ apps/heatmap.py
@@ -11,19 +11,13 @@ def app():
    filepath = "https://raw.githubusercontent.com/JeremyHester/HeatIslandDemo/master/preliminarydata.csv"
    # Read the CSV file into a DataFrame
       #df = pd.read_csv(filepath)
-
    # Generate a correlation matrix for the DataFrame
       #corr_matrix = df.corr()
-
    # Create a heatmap of the correlation matrix using Seaborn
       #fig, ax = plt.subplots(figsize=(10,10))
       #sns.heatmap(corr_matrix, annot=True, cmap='coolwarm', ax=ax)
-
    # Display the heatmap in the app
       #st.pyplot(fig)
-
-
-
     m = leafmap.Map(tiles="stamentoner")
     m = leafmap.Map()
     m.add_basemap("Stamen.Toner")
