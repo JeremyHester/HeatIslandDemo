a34d8610c2f47595987a3412c2ec625f58257aa5
JeremyHester <119526734+JeremyHester@users.noreply.github.com>
2023-04-18 00:13:40 -0500

    Update heatmap.py

--- apps/heatmap.py
+++ apps/heatmap.py
@@ -51,6 +51,37 @@ def app():
 # app()
 
 
+####### "working" color
+
+# Find the first non-zero value for latitude and longitude
+   first_lat = data.loc[data['latitude']!=0]['latitude'].iloc[0]
+   first_long = data.loc[data['longitude']!=0]['longitude'].iloc[0]
+
+    # Create the map centered at the first non-zero latitude and longitude value
+   map_center = [first_lat, first_long]
+   my_map = folium.Map(location=map_center, zoom_start=12)
+
+#    # Add the heatmap layer to the map
+   heat_data = [[row['latitude'], row['longitude'], row['temperature']] for index, row in data.iterrows()]
+   heat_map = folium.plugins.HeatMap(heat_data)
+   heat_map.add_to(my_map)
+    #Save map as HTML file
+   my_map.save('map.html')
+
+   # Load HTML file in Streamlit app
+   with open('map.html', 'r') as f:
+      html = f.read()
+   st.components.v1.html(html, width=700, height=500)
+
+
+
+
+
+
+
+
+
+
 
 
 # import os 
@@ -79,44 +110,45 @@ def app():
    
 #    data = pd.read_csv(filepath)
 
-   # Find the first non-zero value for latitude and longitude
-   first_lat = data.loc[data['latitude']!=0]['latitude'].iloc[0]
-   first_long = data.loc[data['longitude']!=0]['longitude'].iloc[0]
+#    # Find the first non-zero value for latitude and longitude
+#    first_lat = data.loc[data['latitude']!=0]['latitude'].iloc[0]
+#    first_long = data.loc[data['longitude']!=0]['longitude'].iloc[0]
 
-    # Create the map centered at the first non-zero latitude and longitude value
-   map_center = [first_lat, first_long]
-   my_map = folium.Map(location=map_center, zoom_start=15)
+#     # Create the map centered at the first non-zero latitude and longitude value
+#    map_center = [first_lat, first_long]
+#    my_map = folium.Map(location=map_center, zoom_start=15)
    
-#    # Create a LinearColormap
-#    colormap = LinearColormap(colors=['green', 'yellow', 'red'], vmin=0, vmax=120)
+# #    # Create a LinearColormap
+# #    colormap = LinearColormap(colors=['green', 'yellow', 'red'], vmin=0, vmax=120)
 
-#     # Add the colormap to the map
-#    my_map.add_child(colormap)
+# #     # Add the colormap to the map
+# #    my_map.add_child(colormap)
 
-#     # Convert the colormap to a dictionary
-#    colormap_dict = colormap.to_dict()
+# #     # Convert the colormap to a dictionary
+# #    colormap_dict = colormap.to_dict()
 
-#     # Save the map
+# #     # Save the map
+# #    my_map.save('map.html')
+   
+# #Define the color gradient using the brunet colormap
+#    gradient_ranges = [-20.0, 32.0, 50.0, 70.0, 90.0, 120.0]
+#    gradient_colors = [cm.coolwarm(x) for x in range(0, 256, int(256/len(gradient_ranges)-1))]
+
+#    cmap = LinearColormap(colors=gradient_colors, vmin=min(gradient_ranges), vmax=max(gradient_ranges))
+
+# #Add the heatmap layer to the map
+#    heat_data = [[row['latitude'], row['longitude'], row['temperature']] for index, row in data.iterrows()]
+#    #heat_map = folium.plugins.HeatMap(heat_data, gradient=colormap_dict, min_opacity=0.8)
+#    #heat_map = folium.plugins.HeatMap(heat_data, gradient=, min_opacity=0.8)
+#    heat_map.add_to(my_map)
+
+# # Save map as HTML file
 #    my_map.save('map.html')
-   
-#Define the color gradient using the brunet colormap
-   gradient_ranges = [-20.0, 32.0, 50.0, 70.0, 90.0, 120.0]
-   gradient_colors = [cm.coolwarm(x) for x in range(0, 256, int(256/len(gradient_ranges)-1))]
 
-   cmap = LinearColormap(colors=gradient_colors, vmin=min(gradient_ranges), vmax=max(gradient_ranges))
-
-#Add the heatmap layer to the map
-   heat_data = [[row['latitude'], row['longitude'], row['temperature']] for index, row in data.iterrows()]
-   heat_map = folium.plugins.HeatMap(heat_data, gradient=colormap_dict, min_opacity=0.8)
-   heat_map.add_to(my_map)
-
-# Save map as HTML file
-   my_map.save('map.html')
-
-# Load HTML file in Streamlit app
-   with open('map.html', 'r') as f:
-    html = f.read()
-   st.components.v1.html(html, width=700, height=500)
+# # Load HTML file in Streamlit app
+#    with open('map.html', 'r') as f:
+#     html = f.read()
+#    st.components.v1.html(html, width=700, height=500)
    
   
    
