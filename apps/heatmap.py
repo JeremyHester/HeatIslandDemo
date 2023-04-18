114750b03d882e37de8be0c2cd911c34ec8ff936
JeremyHester <119526734+JeremyHester@users.noreply.github.com>
2023-04-17 19:33:30 -0500

    Update heatmap.py

--- apps/heatmap.py
+++ apps/heatmap.py
@@ -21,58 +21,67 @@ def app():
    filepath = "https://raw.githubusercontent.com/JeremyHester/HeatIslandDemo/master/preliminarydata2.csv"
 
    # Load data
-   df = pd.read_csv(filepath)
+   #df = pd.read_csv(filepath)
 
    # Create map object
-   m = folium.Map(location=[df['latitude'].iloc[0], df['longitude'].iloc[0]], zoom_start=15)
+   #m = folium.Map(location=[df['latitude'].iloc[0], df['longitude'].iloc[0]], zoom_start=15)
 
    # Create feature group for path walked
-   path = folium.FeatureGroup(name='Path Walked')
-   locations = list(zip(df['latitude'], df['longitude']))
-   path.add_child(folium.PolyLine(locations=locations, color='blue', weight=5))
-   m.add_child(path)
+   #path = folium.FeatureGroup(name='Path Walked')
+   #locations = list(zip(df['latitude'], df['longitude']))
+   #path.add_child(folium.PolyLine(locations=locations, color='blue', weight=5))
+   #m.add_child(path)
 
    # Create feature group for temperature data
-   temp_data = folium.FeatureGroup(name='Temperature Data')
-   temperatures = list(df['temperature'])
-   cmap = folium.LinearColormap(['blue', 'green', 'yellow', 'red'], vmin=79, vmax=87)
-   for location, temp in zip(locations, temperatures):
-       temp_data.add_child(folium.Marker(location=location, icon=folium.Icon(color=cmap(temp), icon='info-sign')))
-   m.add_child(temp_data)
+   #temp_data = folium.FeatureGroup(name='Temperature Data')
+   #temperatures = list(df['temperature'])
+   #cmap = folium.LinearColormap(['blue', 'green', 'yellow', 'red'], vmin=79, vmax=87)
+   #for location, temp in zip(locations, temperatures):
+    #   temp_data.add_child(folium.Marker(location=location, icon=folium.Icon(color=cmap(temp), icon='info-sign')))
+   #m.add_child(temp_data)
 
    # Add layer control
-   folium.LayerControl().add_to(m)
+   #folium.LayerControl().add_to(m)
 
    # Save map as HTML file
-   m.save('map.html')
+   #m.save('map.html')
+
+   # Load HTML file in Streamlit app
+   #with open('map.html', 'r') as f:
+    #  html = f.read()
+   #st.components.v1.html(html, width=700, height=500)
+   
+
+   
+   
+   
+   
+   data = pd.read_csv(filepath)
+
+    # Find the first non-zero value for latitude and longitude
+    first_lat = data.loc[data['latitude']!=0]['latitude'].iloc[0]
+    first_long = data.loc[data['longitude']!=0]['longitude'].iloc[0]
+
+    # Create the map centered at the first non-zero latitude and longitude value
+    map_center = [first_lat, first_long]
+    my_map = folium.Map(location=map_center, zoom_start=12)
+
+#    # Add the heatmap layer to the map
+    heat_data = [[row['latitude'], row['longitude'], row['temperature']] for index, row in data.iterrows()]
+    heat_map = folium.plugins.HeatMap(heat_data)
+    heat_map.add_to(my_map)
+    #Save map as HTML file
+    m.save('map.html')
 
    # Load HTML file in Streamlit app
    with open('map.html', 'r') as f:
       html = f.read()
    st.components.v1.html(html, width=700, height=500)
    
-
+  
    
-   
-   
-   
-#    data = pd.read_csv(filepath)
-
-#    # Find the first non-zero value for latitude and longitude
-#    first_lat = data.loc[data['latitude']!=0]['latitude'].iloc[0]
-#    first_long = data.loc[data['longitude']!=0]['longitude'].iloc[0]
-
-#    # Create the map centered at the first non-zero latitude and longitude value
-#    map_center = [first_lat, first_long]
-#    my_map = folium.Map(location=map_center, zoom_start=12)
-
-#    # Add the heatmap layer to the map
-#    heat_data = [[row['latitude'], row['longitude'], row['temperature']] for index, row in data.iterrows()]
-#    heat_map = folium.plugins.HeatMap(heat_data)
-#    heat_map.add_to(my_map)
-
 #    # Display the map using Streamlit
-#    st.markdown(my_map._repr_html_(), unsafe_allow_html=True)
+   # st.markdown(my_map._repr_html_(), unsafe_allow_html=True)
 
 
 
