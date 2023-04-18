21fa93b1f441c80ffbb21e7b86353c3c6ba3f9f4
JeremyHester <119526734+JeremyHester@users.noreply.github.com>
2023-04-17 20:45:02 -0500

    Update home.py

--- apps/home.py
+++ apps/home.py
@@ -1,5 +1,12 @@
+import os 
 import streamlit as st
-import leafmap.foliumap as leafmap
+import pandas as pd
+import numpy as np
+import folium
+import folium.plugins as plugins
+from branca.colormap import LinearColormap
+from folium.plugins import HeatMap
+import matplotlib.cm as cm
 
 
 def app():
@@ -11,7 +18,36 @@ def app():
 
     """
     )
+    filepath = "https://raw.githubusercontent.com/JeremyHester/HeatIslandDemo/master/preliminarydata2.csv"
+      # Load data
+    df = pd.read_csv(filepath)
 
-    m = leafmap.Map(locate_control=True)
-    m.add_basemap("ROADMAP")
-    m.to_streamlit(height=700)
+   # Create map object
+    m = folium.Map(location=[df['latitude'].iloc[0], df['longitude'].iloc[0]], zoom_start=15)
+
+   # Create feature group for path walked
+    path = folium.FeatureGroup(name='Path Walked')
+    locations = list(zip(df['latitude'], df['longitude']))
+    path.add_child(folium.PolyLine(locations=locations, color='blue', weight=5))
+    m.add_child(path)
+
+   # Create feature group for temperature data
+    temp_data = folium.FeatureGroup(name='Temperature Data')
+    temperatures = list(df['temperature'])
+    cmap = folium.LinearColormap(['blue', 'green', 'yellow', 'red'], vmin=79, vmax=87)
+    for location, temp in zip(locations, temperatures):
+       temp_data.add_child(folium.Marker(location=location, icon=folium.Icon(color=cmap(temp), icon='info-sign')))
+    m.add_child(temp_data)
+
+   # Add layer control
+    folium.LayerControl().add_to(m)
+
+   # Save map as HTML file
+    m.save('map.html')
+
+   # Load HTML file in Streamlit app
+    with open('map.html', 'r') as f:
+      html = f.read()
+    st.components.v1.html(html, width=700, height=500)
+   
+    
