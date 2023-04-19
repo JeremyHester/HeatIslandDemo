563a26b7698559c3a0351d21574679bb79d9810b
JeremyHester <119526734+JeremyHester@users.noreply.github.com>
2023-04-19 12:54:46 -0500

    Update home.py

--- apps/home.py
+++ apps/home.py
@@ -19,35 +19,36 @@ def app():
     """
     )
     filepath = "https://raw.githubusercontent.com/JeremyHester/HeatIslandDemo/master/preliminarydata2.csv"
-      # Load data
+     # Load data
     df = pd.read_csv(filepath)
 
-   # Create map object
+    # Create map object
     m = folium.Map(location=[df['latitude'].iloc[0], df['longitude'].iloc[0]], zoom_start=15)
 
-   # Create feature group for path walked
+    # Create feature group for path walked
     path = folium.FeatureGroup(name='Path Walked')
     locations = list(zip(df['latitude'], df['longitude']))
     path.add_child(folium.PolyLine(locations=locations, color='blue', weight=5))
     m.add_child(path)
 
-   # Create feature group for temperature data
+    # Create feature group for temperature data
     temp_data = folium.FeatureGroup(name='Temperature Data')
     temperatures = list(df['temperature'])
     cmap = folium.LinearColormap(['blue', 'green', 'yellow', 'red'], vmin=79, vmax=87)
     for location, temp in zip(locations, temperatures):
-       temp_data.add_child(folium.Marker(location=location, icon=folium.Icon(color=cmap(temp), icon='info-sign')))
+     marker = folium.Marker(location=location, icon=folium.Icon(color=cmap(temp), icon='info-sign'))
+     marker.add_child(folium.Popup(f'Temperature: {temp} °F'))
+     temp_data.add_child(marker)
     m.add_child(temp_data)
 
-   # Add layer control
+    # Add layer control
     folium.LayerControl().add_to(m)
 
-   # Save map as HTML file
+    # Save map as HTML file
     m.save('map.html')
 
-   # Load HTML file in Streamlit app
+    # Load HTML file in Streamlit app
     with open('map.html', 'r') as f:
-      html = f.read()
+     html = f.read()
     st.components.v1.html(html, width=700, height=500)
-   
     
