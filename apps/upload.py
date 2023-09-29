39416ae3902f2a96e2e78393b7dd66ef625d82b6
JeremyHester <119526734+JeremyHester@users.noreply.github.com>
2023-09-29 13:46:13 -0500

    Update upload.py

--- apps/upload.py
+++ apps/upload.py
@@ -18,7 +18,7 @@ def app():
     container = st.container()
     with container:
     # Define columns
-     left_column, right_column = st.beta_columns(2)
+     left_column, right_column = st.columns(2)
     # Display DataFrame in the left column
     with left_column:
         st.write(df, height=500)
